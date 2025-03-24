80f33a29eab8c393c1007ef19f023370a7154057
YanivCohenC <yaniv.cohen.c@gmail.com>
2025-03-24 11:18:00 +0200

    Changed YanivCohen.java print to a new print

--- YanivCohen.java
+++ YanivCohen.java
@@ -1,7 +1,7 @@
 public class YanivCohen {
 	public static void main (String[] args) {
 		for (int i=1; i<=10; i++) {
-			System.out.println("Yaniv Cohen ..." + i);
+			System.out.println("Yaniv Cohen New Print..." + i);
 		}
 	}
 }
